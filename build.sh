612cccbafe918de75ef6ea2ebe471e3d1652f7ee
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2024-02-08 00:14:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ls
 curl -L https://raw.githubusercontent.com/spiritLHLS/honeygain-one-click-command-installation/main/hg.sh -o hg.sh && chmod +x hg.sh && bash hg.sh -m 你的邮箱 -p 你的密码
 ls
-man hg.sh
+cat hg.sh
