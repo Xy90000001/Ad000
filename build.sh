f347aab304d46ca25af06b4b7b3431ab11b3af7d
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2024-02-08 09:22:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ ls
 curl -L https://raw.githubusercontent.com/spiritLHLS/honeygain-one-click-command-installation/main/hg.sh -o hg.sh && chmod +x hg.sh && bash hg.sh -m 你的邮箱 -p 你的密码
 ls
 cat hg.sh
+docker run honeygain/honeygain -tou-accept -email xy90000001@gmail.com -pass #HONEYGAINx9 -device DEVICE_NAMEq
